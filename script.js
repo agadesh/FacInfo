0219f0c23833a6515e09b10cf356e1657ecce3df
Adesh Agarwal <adeshagarwal440@gmail.com>
2020-10-08 16:45:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('signchg').addEventListener('click', function (e) {
+  document.getElementById('loginid').classList.toggle('none');
+  document.getElementById('signupid').classList.toggle('none');
+});
+document.getElementById('logchg').addEventListener('click', function (e) {
+  document.getElementById('loginid').classList.toggle('none');
+  document.getElementById('signupid').classList.toggle('none');
+});
